4ce6be971586033874ee220d31e07628781bf1fd
Abdulwadod <ai.wadod@hotmail.com>
2026-05-26 19:37:12 +0300

    Initial commit for GDGOC Eid Project

--- src/componans/Footer.jsx
+++ src/componans/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = ({classs=""}) => {
     return (
         <footer className="foot">
             {/* <a href='https://linktr.ee/ai.wadod' target='_blank'>تصميم و تطوير <span>ودود</span></a> */}
-            <a href='https://linktr.ee/ai.wadod' target='_blank'>تصميم و تطوير <img style={{ marginBottom: "-5px", width: "50px" }} src="footer.png" alt="footer" /></a>
+            <a href='https://linktr.ee/ai.wadod' target='_blank'>تصميم و تطوير <img style={{ marginBottom: "-5px", width: "60px",marginRight:"2px" }} src="footer.png" alt="footer" /></a>
         </footer>
     );
 };
